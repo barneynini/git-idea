825d7c20c68bdfbdea22145357da4585fe5e5dea
barney <liuyanling1989110@163.com>
2022-04-08 16:05:57 +0800

    second

---
+++ test.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+class TestDemo:
+    def test_1(self):
+        assert 1 == 1
+
+    def test_2(self):
+        assert 1 == 21
+
+
+if __name__ == '__main__':
+    pytest.main(['-sq', 'test.py'])
